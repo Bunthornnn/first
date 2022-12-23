04f56ae41a6262f4f49b1aa3b867add6915873e8
thorn <thonbunthorn77@gmail.com>
2022-12-23 18:37:04 +0700

    aaa

--- 25-create-cookie.php
+++ 25-create-cookie.php
@@ -1,4 +1,5 @@
 <?php 
-    setcookie('ABC',100,time()+(86400*30));
-    echo $_COOKIE['ABC'];
+    // setcookie('ABC',100,time()+(86400*30));
+    setcookie('abc',"Hello my girl",time()+(86400*7));
+    echo $_COOKIE['abc'];
 ?>
